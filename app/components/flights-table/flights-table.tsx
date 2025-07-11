8ce472258ffaa58575bfd6e23a9507c351888412
Daniell Wijdenbosch <d.wijdenbosch@icloud.com>
2025-07-11 20:28:20 +0200

    refactor: split components

---
+++ app/components/flights-table/flights-table.tsx
@@ -0,0 +1,123 @@
+import "./flights-table.css";
+import {
+  AccessorKeyColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import type { flights } from "../../flights.json";
+import { useState } from "react";
+import { fuzzyFilter } from "lib/util/table-filters";
+import { DebouncedInput } from "../debounced-input";
+
+type FlightsTableProps = {
+  flights: typeof flights;
+  columns: AccessorKeyColumnDef<(typeof flights)[number], string>[];
+  maxRows?: number;
+};
+
+export function FlightsTable({
+  maxRows = 5,
+  columns,
+  flights,
+}: FlightsTableProps) {
+  const [globalFilter, setGlobalFilter] = useState("");
+
+  const table = useReactTable({
+    data: flights,
+    columns,
+    filterFns: {
+      fuzzy: fuzzyFilter,
+    },
+    initialState: {
+      sorting: [
+        {
+          id: "expectedTime",
+          desc: false,
+        },
+      ],
+    },
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onGlobalFilterChange: setGlobalFilter,
+    state: {
+      globalFilter,
+    },
+  });
+
+  const airportColumn = table.getColumn("airport");
+  const filterValue = airportColumn?.getFilterValue() as string | undefined;
+
+  return (
+    <>
+      <div className="center">
+        <DebouncedInput
+          className="flights-search-input"
+          type="text"
+          value={filterValue ?? ""}
+          onChange={(value) => airportColumn?.setFilterValue(value)}
+          placeholder="Enter your destination"
+        />
+      </div>
+
+      {filterValue?.length && filterValue.length >= 3 && (
+        <table className="center">
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th key={header.id} colSpan={header.colSpan}>
+                      {header.isPlaceholder ? null : (
+                        <>
+                          <div
+                            aria-hidden="true"
+                            {...{
+                              className: header.column.getCanSort()
+                                ? "cursor-pointer select-none"
+                                : "",
+                              onClick: header.column.getToggleSortingHandler(),
+                            }}
+                          >
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                            {{
+                              asc: " 🔼",
+                              desc: " 🔽",
+                            }[header.column.getIsSorted() as string] ?? null}
+                          </div>
+                        </>
+                      )}
+                    </th>
+                  );
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table
+              .getRowModel()
+              .rows.slice(0, maxRows)
+              .map((row) => (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
 import { flights as flightsData } from "../flights.json";
 import { useLoaderData } from "@remix-run/react";
-import {
-  createColumnHelper,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-import { DebouncedInput } from "~/components/debounced-input";
-import { fuzzyFilter } from "lib/util/table-filters";
+import { createColumnHelper } from "@tanstack/react-table";
+import { FlightsTable } from "~/components/flights-table/flights-table";
 
 export const meta: MetaFunction = () => {
   return [
@@ -26,131 +18,42 @@ export const loader = async () => {
 const columnHelper =
   createColumnHelper<Awaited<ReturnType<typeof loader>>[number]>();
 
-const MAX_TABLE_ROWS = 5;
+const columns = [
+  {
+    accessorKey: "airport",
+    filterFn: "fuzzy" as const,
+  },
+  columnHelper.accessor("flightNumber", {
+    header: () => <span>Flight</span>,
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor("date", {
+    header: () => <span>Date</span>,
+    cell: (info) => info.getValue(),
+    sortingFn: "datetime",
+  }),
+  columnHelper.accessor("originalTime", {
+    header: () => <span>Original Departure</span>,
+    cell: (info) => info.getValue(),
+    sortingFn: "datetime",
+  }),
+  columnHelper.accessor("expectedTime", {
+    header: () => <span>Expected Departure</span>,
+    cell: (info) => info.getValue(),
+    sortingFn: "datetime",
+  }),
+];
 
 export default function Index() {
   const flights = useLoaderData<typeof loader>();
-
-  const columns = [
-    {
-      accessorKey: "airport",
-      filterFn: "fuzzy" as const,
-    },
-    columnHelper.accessor("flightNumber", {
-      header: () => <span>Flight</span>,
-      cell: (info) => info.getValue(),
-    }),
-    columnHelper.accessor("date", {
-      header: () => <span>Date</span>,
-      cell: (info) => info.getValue(),
-      sortingFn: "datetime",
-    }),
-    columnHelper.accessor("originalTime", {
-      header: () => <span>Original Departure</span>,
-      cell: (info) => info.getValue(),
-      sortingFn: "datetime",
-    }),
-    columnHelper.accessor("expectedTime", {
-      header: () => <span>Expected Departure</span>,
-      cell: (info) => info.getValue(),
-      sortingFn: "datetime",
-    }),
-  ];
-
-  const table = useReactTable({
-    data: flights,
-    columns,
-    filterFns: {
-      fuzzy: fuzzyFilter,
-    },
-    initialState: {
-      sorting: [
-        {
-          id: "expectedTime",
-          desc: false,
-        },
-      ],
-    },
-    getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-  });
-
-  const column = table.getColumn("airport")!;
-  const filterValue = column.getFilterValue() as string | undefined;
 
   return (
     <main>
       <div className="card-container center">
         <section>
           <h2 className="card-title">Flight departure information</h2>
-          <div className="center">
-            <DebouncedInput
-              className="flights-search-input"
-              type="text"
-              value={filterValue ?? ""}
-              onChange={(value) => column.setFilterValue(value)}
-              placeholder="Enter your destination"
-            />
-          </div>
 
-          {filterValue?.length && filterValue.length >= 3 && (
-            <table className="center">
-              <thead>
-                {table.getHeaderGroups().map((headerGroup) => (
-                  <tr key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => {
-                      return (
-                        <th key={header.id} colSpan={header.colSpan}>
-                          {header.isPlaceholder ? null : (
-                            <>
-                              <div
-                                aria-hidden="true"
-                                {...{
-                                  className: header.column.getCanSort()
-                                    ? "cursor-pointer select-none"
-                                    : "",
-                                  onClick:
-                                    header.column.getToggleSortingHandler(),
-                                }}
-                              >
-                                {flexRender(
-                                  header.column.columnDef.header,
-                                  header.getContext()
-                                )}
-                                {{
-                                  asc: " 🔼",
-                                  desc: " 🔽",
-                                }[header.column.getIsSorted() as string] ??
-                                  null}
-                              </div>
-                            </>
-                          )}
-                        </th>
-                      );
-                    })}
-                  </tr>
-                ))}
-              </thead>
-              <tbody>
-                {table
-                  .getRowModel()
-                  .rows.slice(0, MAX_TABLE_ROWS)
-                  .map((row) => (
-                    <tr key={row.id}>
-                      {row.getVisibleCells().map((cell) => (
-                        <td key={cell.id}>
-                          {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext()
-                          )}
-                        </td>
-                      ))}
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          )}
+          <FlightsTable columns={columns} flights={flights} />
         </section>
       </div>
     </main>
